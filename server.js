725e51c933b4df1516ccd9a54deef78612b77b50
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-10 18:06:48 -0500

    created server.js and imported all necessary packages and initialized handlebars engine

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize');
+
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'this is super duper secret',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, //expires after 24 hours
+    },
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//initialize express-session
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes);
+
+sequelize.sync( {force: true }).then(() => {
+    app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
+})
