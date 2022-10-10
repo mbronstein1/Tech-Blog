6770ae6060df1f12e7a74d8eea981e8b88391104
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-10 18:23:04 -0500

    created models

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
+Post.init (
+    {
+        msg: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
