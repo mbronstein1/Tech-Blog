695c9153d883825a981f7cbba0f2b16d099cd356
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-10 18:52:26 -0500

    created home http route

---
+++ controllers/home-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            include: [
+                {
+                    model: User
+                },
+            ],
+        })
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render('home', { 
+            posts,
+            loggedIn: req.session.loggedIn
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync( {force: true }).then(() => {
     app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
