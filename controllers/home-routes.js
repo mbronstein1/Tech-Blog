4ca1b25255d9e0cf6f31800bd43cc47d2ccaff2f
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-10 20:51:59 -0500

    commented out /dashboard route for now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,25 +34,25 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/dashboard', async (req, res) => {
-    try {
-        const dbPostData = await Post.findAll({
-            include: [
-                {
-                    model: User
-                },
-            ],
-        })
-        const posts = dbPostData.map((post) => post.get({ plain: true }));
-        console.log(posts)
-        res.render('home', { 
-            posts,
-            loggedIn: req.session.loggedIn
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+// router.get('/dashboard', async (req, res) => {
+//     try {
+//         const dbPostData = await Post.findAll({
+//             include: [
+//                 {
+//                     model: User
+//                 },
+//             ],
+//         })
+//         const posts = dbPostData.map((post) => post.get({ plain: true }));
+//         console.log(posts)
+//         res.render('home', { 
+//             posts,
+//             loggedIn: req.session.loggedIn
+//         })
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
