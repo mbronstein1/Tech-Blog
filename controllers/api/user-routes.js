7f4996172f216e19c241d2f674187e6aabcea96e
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-10 22:44:10 -0500

    created dashboard page and worked through some more routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData)
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//Login
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username
+            },
+        });
+
+        //Check is user exists in db or username is correct
+        if(!dbUserData) {
+            res.status(400).json({message: 'Incorrect email or password. Please try again.'});
+            return;
+        };
+
+        //Check is password is correct
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect email or password. Please try again.'});
+            return;
+        };
+
+        //Approve authorization, store session, and create cookie
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            console.log("Cookie:", req.session.cookie);
+            res.status(200).json({
+                user: dbUserData,
+                message: 'You are now logged in!'});
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
+
+//Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
+
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,16 +3,18 @@ const { User, Post } = require('../models');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
-hbs.handlebars.registerHelper('ifCond', function(v1, v2, options) {
-    if(v1 === v2) {
-        console.log(options.fn(this))
+//Handlebars custom helper function for comparison operator
+hbs.handlebars.registerHelper('ifCond', function (v1, v2, options) {
+    if (v1 === v2) {
+        // console.log(options.fn(this))
         return options.fn(this)
     } else {
-        console.log(options.inverse(this))
+        // console.log(options.inverse(this))
         return options.inverse(this)
     }
 })
 
+//GET route for homepage
 router.get('/', async (req, res) => {
     try {
         const dbPostData = await Post.findAll({
@@ -23,10 +25,10 @@ router.get('/', async (req, res) => {
             ],
         })
         const posts = dbPostData.map((post) => post.get({ plain: true }));
-        console.log(posts)
-        res.render('home', { 
+        // console.log(posts)
+        res.render('home', {
             posts,
-            loggedIn: req.session.loggedIn
+            isLoggedIn: req.session.loggedIn
         })
     } catch (err) {
         console.log(err);
@@ -34,25 +36,20 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.get('/dashboard', async (req, res) => {
-//     try {
-//         const dbPostData = await Post.findAll({
-//             include: [
-//                 {
-//                     model: User
-//                 },
-//             ],
-//         })
-//         const posts = dbPostData.map((post) => post.get({ plain: true }));
-//         console.log(posts)
-//         res.render('home', { 
-//             posts,
-//             loggedIn: req.session.loggedIn
-//         })
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
+//GET route for dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        isLoggedIn: req.session.loggedIn
+    })
+});
+
+//GET route for login page
+router.get('/login', (res, req) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+})
 
 module.exports = router;
